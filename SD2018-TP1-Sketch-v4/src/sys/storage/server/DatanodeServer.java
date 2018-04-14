4e473c3dc618dc946e477a0e7d06a803b6de3db2
Andrefrf <andrefiliperferreira@hotmail.com>
2018-04-14 18:52:30 +0100

    updated 14/04 18h52

--- SD2018-TP1-Sketch-v4/src/sys/storage/server/DatanodeServer.java
+++ SD2018-TP1-Sketch-v4/src/sys/storage/server/DatanodeServer.java
@@ -1,10 +1,14 @@
 package sys.storage.server;
 
+import java.awt.geom.IllegalPathStateException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.URI;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
@@ -24,8 +28,9 @@ public class DatanodeServer implements Datanode {
 	private static Logger logger = Logger.getLogger(Datanode.class.toString());
 	private static final int INITIAL_SIZE = 32;
 	private Map<String, byte[]> blocks = new HashMap<>(INITIAL_SIZE);
-	private static final String URI_BASE = "http://" + IP.hostAddress() + ":9000/";
+	private static final String URI_BASE = "http://" + IP.hostAddress() + ":8500/";
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws IOException {
 
 		System.setProperty("java.net.preferIPv4Stack", "true");
@@ -36,6 +41,7 @@ public class DatanodeServer implements Datanode {
 		InetAddress group = InetAddress.getByName("225.9.0.1");
 		URI serverURI = URI.create(URI_BASE);
 		JdkHttpServerFactory.createHttpServer(serverURI, config);
+		System.out.println(serverURI);
 
 		if (!group.isMulticastAddress()) {
 			System.out.println("Not a multicast address (use range : 224.0.0.0 -- 239.255.255.255)");
@@ -43,15 +49,15 @@ public class DatanodeServer implements Datanode {
 		}
 
 		System.err.println("Server ready....");
-		try{
+		System.out.println(serverURI);
+		try {
 			MulticastSocket socket = new MulticastSocket(9000);
 			socket.joinGroup(group);
 			while (true) {
 				byte[] buffer = new byte[MAX_DATAGRAM_SIZE];
 				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
-				
+
 				socket.receive(request);
-				System.out.println("Hello");
 				if (!request.getData().equals(PATH)) {
 					continue;
 				}
@@ -60,34 +66,62 @@ public class DatanodeServer implements Datanode {
 				socket.send(request);
 			}
 		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
 	}
 
 	@Override
 	public synchronized String createBlock(byte[] data) {
 		String id = Random.key64();
-		blocks.put(id, data);
-		return URI_BASE + "datanote/" + id;
+		if (blocks.putIfAbsent(id, data) != null) {
+			throw new WebApplicationException(Status.CONFLICT);
+		} else {
+			String url = "http://" + IP.hostAddress() + ":8500/" + "datanode/" + id;
+			try {
+				FileOutputStream file = new FileOutputStream(id);
+				file.write(data);
+				System.out.println(id);
+			} catch (Exception e) {
+
+			}
+
+			return url;
+		}
 	}
 
 	@Override
 	public synchronized void deleteBlock(String block) {
-		if(blocks.containsKey(block)) {
+		if (blocks.containsKey(block)) {
 			blocks.remove(block);
 			throw new WebApplicationException(Status.NO_CONTENT);
-		}else {
+		} else {
 			throw new WebApplicationException(Status.NOT_FOUND);
 		}
 	}
 
 	@Override
 	public byte[] readBlock(String block) {
-		byte[] data = blocks.get(block);
-		if (data != null) {
-			return data;
+
+		byte[] data = null;
+		byte[] g = null;
+
+		try {
+			FileInputStream f = new FileInputStream(block);
+			while (f.read(g) != 0) {
+				data = g;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		else {
-			throw new WebApplicationException(Status.NOT_FOUND);
-		}
+		return data;
+		
+		
+//		 byte[] data = blocks.get(block);
+//		 System.out.println(block);
+//		 if (data != null) {
+//		 return data;
+//		 } else {
+//		 throw new WebApplicationException(Status.NOT_FOUND);
+//		 }
 	}
 }
